bc16369674765dffbe488b797d6c2abb8b89ed98
praslea <pruna_mihai@yahoo.com>
2020-11-03 20:58:34 +0200

    version 1

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Python_Email_Notification
-POC Sending email notification when server is down
+
+POC Sending email notification when the server is down
+
+### Project structure
+  
+* requirements.txt  
+* environment.yml  
+* python script  
+
+```
+├── email_sender.py
+├── environment.yml
+├── README.md
+└── requirements.txt
+```
+
+### Env Variables
+
+* sender email address  
+* recipient address  
+* smtp server 
+
+
+### Docker setup
+
+Create a docker service with tree nodes. The requests will be answered the proxy.  
+Make the the http port available.  
+
+```
+docker swam init .
+(docker join swarm?)
+docker service create -p 8088:80 --name web nginx:1.13.7
+docker service update -publish-add 8080:80
+```
+### Docker cmds
+
+```
+docker service ps web
+docker service ls
+docker network inspect # swarm network created should have a swarm scope
+```
+
---
+++ email_sender.py
@@ -0,0 +1,103 @@
+from socket import gaierror
+import requests
+import smtplib
+import csv
+import os
+
+from requests.exceptions import ConnectTimeout
+from requests.exceptions import ConnectionError
+
+from email.header import Header
+from email.utils import formataddr
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from time import sleep
+#from daemonize import Daemonize
+
+
+# Setup pid file
+
+#pid = "/home/Bitbucket/web_scraping/email_automat/test.pid"
+
+def web_availability():
+    #url="http://172.22.0.2:8080"
+    url="http://192.168.100.14:8088/blah"
+    try:
+        response = requests.get(url, timeout=3)
+        status = response.status_code
+        headers = response.headers
+
+        # format html rep to user friendly string
+        headers = str(headers).replace("\'", "").replace("{", "").replace("}", "")
+        
+    except (ConnectionError, ConnectTimeout):
+        print("connection error")
+        headers = "Connection Error"
+        status = "No Status/Site might be down"
+
+    return headers, status
+
+def send_email(sender, passwd, recipient, headers, status, count):
+
+    ### Server Details
+    smtp_server=os.environ['SMTP_SERVER']
+    port=465
+
+    ### Msg creation
+    message = "Response Headers: "+ str(headers)+ "\n"+"Status: " + str(status) + "\n" + "Missing response count: " + str(count)
+
+    msg = MIMEMultipart()
+    msg['Subject'] = Header("Sent from python", 'utf-8')
+    sender_title = "Python Automated Email notification"
+    msg['From'] = formataddr((str(Header(sender_title, 'utf-8')), sender))
+    msg['To'] = recipient
+    msg.attach(MIMEText(message))
+
+    if status !=200:
+
+        count+=1
+        try:
+            # send your message with credentials specified above
+
+            server = smtplib.SMTP_SSL(smtp_server, 465)
+            server.login(sender, passwd)
+            server.sendmail(sender, [recipient], msg.as_string())
+            server.quit()
+
+        # tell the script to report if your message was sent or which errors need to be fixed 
+            print('Email notification sent')
+        except (gaierror, ConnectionRefusedError):
+            print('Failed to connect to the server. Bad connection settings?')
+        except smtplib.SMTPServerDisconnected:
+            print('Failed to connect to the server. Wrong user/password?')
+        except smtplib.SMTPException as e:
+            print('SMTP error occurred: ' + str(e))
+    else:
+        flag = False
+
+    return count, flag
+        
+
+#if __name__ == "__main__":
+def main():
+    count = 0
+    flag = True
+    while True:
+
+        # Define mail sender / recipient
+
+        sender = os.environ['SENDER']
+        recipient = os.environ['RECIPIENT']
+        passwd = os.environ['PASSWORD']
+
+        headers, status = web_availability()
+        count = send_email(sender, passwd, recipient, headers, status, count)
+        
+        print(count)
+        sleep(10)
+
+
+#daemon = Daemonize(app="test_app", pid=pid, action=main)
+#daemon.start()
+main()
